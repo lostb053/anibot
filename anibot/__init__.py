cc203a165b7b17cb3da18691da2570313edaa5e6
Lucky Jain <72547660+lostb053@users.noreply.github.com>
2022-01-06 11:53:46 +0530

    Update __init__.py

--- anibot/__init__.py
+++ anibot/__init__.py
@@ -10,8 +10,8 @@ DB_URL = os.environ.get("DATABASE_URL")
 ANILIST_CLIENT = os.environ.get("ANILIST_CLIENT")
 ANILIST_SECRET = os.environ.get("ANILIST_SECRET")
 ANILIST_REDIRECT_URL = os.environ.get("ANILIST_REDIRECT_URL", "https://anilist.co/api/v2/oauth/pin")
-API_ID = int(os.environ.get("API_ID")
-LOG_CHANNEL_ID = int(os.environ.get("LOG_CHANNEL_ID")
+API_ID = int(os.environ.get("API_ID"))
+LOG_CHANNEL_ID = int(os.environ.get("LOG_CHANNEL_ID"))
 OWNER = list(filter(lambda x: x, map(int, os.environ.get("OWNER_ID", "1005170481 804248372 1993696756").split())))  ## sudos can be included
 
 DOWN_PATH = "anibot/downloads/"
